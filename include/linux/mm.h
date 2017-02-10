df5ce7966348f535aea1165145dfc91fc11c7c01
Nathan Chancellor <natechancellor@gmail.com>
2017-02-10 02:07:04 -0700

    block: set readahead to 512
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2246,7 +2246,7 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
+#define VM_MAX_READAHEAD	512	/* kbytes */
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
