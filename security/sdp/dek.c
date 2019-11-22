e3cbf2ff6e4d67fceccba0bfcf5a83fd7b432f84
Denis Efremov <efremov@linux.com>
2019-11-23 00:21:23 +0300

    security/sdp/dek: fix printk format errors
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- security/sdp/dek.c
+++ security/sdp/dek.c
@@ -1093,7 +1093,7 @@ static long dek_do_ioctl_req(unsigned int minor, unsigned int cmd,
 		memset(tempPlain_dek->buf, 0, DEK_MAXLEN);
 
 		if (ret < 0) {
-			DEK_LOGE("DEK_ENCRYPT_DEK: failed to encrypt dek! (err:%d)\n", ret);
+			DEK_LOGE("DEK_ENCRYPT_DEK: failed to encrypt dek! (err:%ld)\n", ret);
 			zero_out((char *)&req, sizeof(dek_arg_encrypt_dek));
 			kzfree(tempPlain_dek);
 			kzfree(tempEnc_dek);
@@ -1162,7 +1162,7 @@ static long dek_do_ioctl_req(unsigned int minor, unsigned int cmd,
 				tempEnc_dek, tempPlain_dek);
 
 		if (ret < 0) {
-			DEK_LOGE("DEK_DECRYPT_DEK: failed to decrypt dek! (err:%d)\n", ret);
+			DEK_LOGE("DEK_DECRYPT_DEK: failed to decrypt dek! (err:%ld)\n", ret);
 			zero_out((char *)&req, sizeof(dek_arg_decrypt_dek));
 			kzfree(tempPlain_dek);
 			kzfree(tempEnc_dek);
