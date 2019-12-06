71ece85a90254786acda42530bf7462338266dc4
Denis Efremov <efremov@linux.com>
2019-12-06 16:16:14 +0300

    sound/usb/card: fix missing printk arguments
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -209,7 +209,7 @@ static int snd_usb_create_stream(struct snd_usb_audio *chip, int ctrlif, int int
 		usb_driver_claim_interface(&usb_audio_driver, iface, (void *)-1L);
 	}
 
-	dev_info(&dev->dev, "usb_host : %s %u:%d \n", __func__);
+	dev_info(&dev->dev, "usb_host : %s %u:%d \n", __func__, ctrlif, interface);
 	return 0;
 }
 
