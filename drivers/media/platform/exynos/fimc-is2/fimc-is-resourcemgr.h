912e650b83e11f1ffde26339eef5033f88892bde
Denis Efremov <efremov@linux.com>
2020-01-21 18:07:53 +0300

    exynos/fimc-is2: fix section mismatch for fimc_is_resourcemgr_probe()
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/media/platform/exynos/fimc-is2/fimc-is-resourcemgr.h
+++ drivers/media/platform/exynos/fimc-is2/fimc-is-resourcemgr.h
@@ -158,7 +158,7 @@ struct fimc_is_resourcemgr {
 	u32					streaming_cnt;
 };
 
-int fimc_is_resourcemgr_probe(struct fimc_is_resourcemgr *resourcemgr, void *private_data, struct platform_device *pdev);
+int __init fimc_is_resourcemgr_probe(struct fimc_is_resourcemgr *resourcemgr, void *private_data, struct platform_device *pdev);
 int fimc_is_resource_open(struct fimc_is_resourcemgr *resourcemgr, u32 rsc_type, void **device);
 int fimc_is_resource_get(struct fimc_is_resourcemgr *resourcemgr, u32 rsc_type);
 int fimc_is_resource_put(struct fimc_is_resourcemgr *resourcemgr, u32 rsc_type);
