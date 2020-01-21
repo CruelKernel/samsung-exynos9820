c2d564dfb8b48bb61bd4147652ae83703d44dd7f
Denis Efremov <efremov@linux.com>
2020-01-22 00:04:54 +0300

    drivers: kperfmon: suppress missing files warnings
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- include/linux/ologk.h
+++ include/linux/ologk.h
@@ -2,13 +2,11 @@
 #define _OLOG_KERNEL_H_
 
 #include <linux/unistd.h>
-#include "olog.pb.h"
 
 #define OLOG_CPU_FREQ_FILTER   1500000
 
-#define ologk(...) _perflog(PERFLOG_LOG, PERFLOG_UNKNOWN, __VA_ARGS__)
-#define perflog(...) _perflog(PERFLOG_LOG, __VA_ARGS__)
-extern void _perflog(int type, int logid, const char *fmt, ...);
-extern void perflog_evt(int logid, int arg1);
+#define ologk(...)
+#define perflog(...)
+#define perflog_evt(...)
 
 #endif
