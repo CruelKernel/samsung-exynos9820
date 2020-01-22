44388dc06bca39869e44b94f2c278ab54c00784d
Denis Efremov <efremov@linux.com>
2020-01-22 15:58:52 +0300

    npu: generated: fix update_ver_info.sh script
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/vision/npu/generated/update_ver_info.sh
+++ drivers/vision/npu/generated/update_ver_info.sh
@@ -9,17 +9,17 @@ if [ -z "$NPU_GIT_LOCAL_CHANGE" ]
 then
 	NPU_GIT_LOCAL_CHANGE="No local change"
 fi
-STASH_DEPTH=`git stash list | wc -l`
-USER_INFO=whoami|sed 's/\\/\-/g'
+STASH_DEPTH="$(git stash list | wc -l)"
+USER_INFO="$(whoami | sed 's/\\/\-/g')"
 
 # Error checking
-if [ ( -z $NPU_GIT_LOG ) -o ( -z $NPU_GIT_HASH ) -o ( -z $USER_INFO ) -o ( -z $NPU_GIT_LOCAL_CHANGE ) -o ( -z $STASH_DEPTH ) ]
+if [ \( -z "$NPU_GIT_LOG" \) -o \( -z "$NPU_GIT_HASH" \) -o \( -z "$USER_INFO" \) -o \( -z "$NPU_GIT_LOCAL_CHANGE" \) -o \( -z "$STASH_DEPTH" \) ]
 then
 	echo "An error occured during build info gathering." >&2
 	exit 16
 fi
 
-BUILD_INFO="$(USER_INFO)@$(hostname) / Build on $(date --rfc-3339='seconds')"
+BUILD_INFO="$USER_INFO@$(hostname) / Build on $(date --rfc-3339='seconds')"
 
 cat > $TARGET_FILE << ENDL
 const char *npu_git_log_str =
