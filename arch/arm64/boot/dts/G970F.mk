02d094fd5c5f47fa629204579d599d5bd010fa58
Denis Efremov <efremov@linux.com>
2020-02-19 20:15:27 +0300

    G970F - BSKO
    Signed-off-by: Denis Efremov <efremov@linux.com>

---
+++ arch/arm64/boot/dts/G970F.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+dtb-y += exynos/exynos9820.dtb
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_17.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_18.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_19.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_20.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_22.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_24.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_25.dtbo
+
+targets += dtbs
+DTB_LIST  := $(dtb-y) $(dtbo-y)
+always    := $(DTB_LIST)
+
+dtbs: $(addprefix $(obj)/, $(DTB_LIST))
+
+clean-files := *.dtb*
