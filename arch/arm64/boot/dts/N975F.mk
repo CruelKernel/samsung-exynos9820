aeec95ae0cb944a5c32386ad21e2edbf3a21984c
Denis Efremov <efremov@linux.com>
2020-02-20 18:56:49 +0300

    N975F - ASGO
    Signed-off-by: Denis Efremov <efremov@linux.com>

---
+++ arch/arm64/boot/dts/N975F.mk
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: GPL-2.0
+dtb-y += exynos/exynos9825.dtb
+dtbo-y += samsung/exynos9820-d2_eur_open_02.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_16.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_17.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_18.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_19.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_20.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_21.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_22.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_23.dtbo
+dtbo-y += samsung/exynos9820-d2_eur_open_24.dtbo
+
+targets += dtbs
+DTB_LIST  := $(dtb-y) $(dtbo-y)
+always    := $(DTB_LIST)
+
+dtbs: $(addprefix $(obj)/, $(DTB_LIST))
+
+clean-files := *.dtb*
