65bb227461239259ee2d0d72a90b692b0723a1dd
xuanhoa8687 <60789352+xuanhoa8687@users.noreply.github.com>
2020-03-10 19:36:40 +0700

    Update npu-ver-info.c

--- drivers/vision/npu/generated/npu-ver-info.c
+++ drivers/vision/npu/generated/npu-ver-info.c
@@ -1,1 +1,3 @@
-#include "npu-ver-info~"
+const char *npu_git_log_str = "0000000";
+const char *npu_git_hash_str = "0000000";
+const char *npu_build_info = "ivanmeler@haxynox / Build on 2019-12-12 23:14:52+02:00";
