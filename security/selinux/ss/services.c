26625e0fd65719090883091fb417be07d3fcd58d
Denis Efremov <efremov@linux.com>
2020-03-11 17:18:51 +0300

    security: selinux: make audit optional
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -467,6 +467,7 @@ mls_ops:
 	return s[0];
 }
 
+#ifdef CONFIG_AUDIT
 /*
  * security_dump_masked_av - dumps masked permissions during
  * security_compute_av due to RBAC, MLS/Constraint and Type bounds.
@@ -556,6 +557,7 @@ out:
 
 	return;
 }
+#endif
 
 /*
  * security_boundary_permission - drops violated permissions
@@ -609,9 +611,11 @@ static void type_attribute_bounds_av(struct context *scontext,
 	/* mask violated permissions */
 	avd->allowed &= ~masked;
 
+#ifdef CONFIG_AUDIT
 	/* audit masked permissions */
 	security_dump_masked_av(scontext, tcontext,
 				tclass, masked, "bounds");
+#endif
 }
 
 /*
@@ -753,6 +757,7 @@ static int security_validtrans_handle_fail(struct context *ocontext,
 					   struct context *tcontext,
 					   u16 tclass)
 {
+#ifdef CONFIG_AUDIT
 	char *o = NULL, *n = NULL, *t = NULL;
 	u32 olen, nlen, tlen;
 
@@ -770,6 +775,7 @@ out:
 	kfree(o);
 	kfree(n);
 	kfree(t);
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -930,6 +936,7 @@ int security_bounded_transition(u32 old_sid, u32 new_sid)
 		index = type->bounds;
 	}
 
+#ifdef CONFIG_AUDIT
 	if (rc) {
 		char *old_name = NULL;
 		char *new_name = NULL;
@@ -949,6 +956,7 @@ int security_bounded_transition(u32 old_sid, u32 new_sid)
 		kfree(new_name);
 		kfree(old_name);
 	}
+#endif
 out:
 	read_unlock(&policy_rwlock);
 
@@ -1542,6 +1550,7 @@ static int compute_sid_handle_invalid_context(
 	u16 tclass,
 	struct context *newcontext)
 {
+#ifdef CONFIG_AUDIT
 	char *s = NULL, *t = NULL, *n = NULL;
 	u32 slen, tlen, nlen;
 
@@ -1561,6 +1570,7 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -2886,8 +2896,6 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 	struct context *context1;
 	struct context *context2;
 	struct context newcon;
-	char *s;
-	u32 len;
 	int rc;
 
 	rc = 0;
@@ -2927,6 +2935,10 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 	if (!policydb_context_isvalid(&policydb, &newcon)) {
 		rc = convert_context_handle_invalid_context(&newcon);
 		if (rc) {
+#ifdef CONFIG_AUDIT
+			char *s;
+			u32 len;
+
 			if (!context_struct_to_string(&newcon, &s, &len)) {
 				audit_log(current->audit_context,
 					  GFP_ATOMIC, AUDIT_SELINUX_ERR,
@@ -2934,6 +2946,7 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 					  "invalid_context=%s", s);
 				kfree(s);
 			}
+#endif
 			goto out_unlock;
 		}
 	}
@@ -3156,6 +3169,7 @@ int security_policycap_supported(unsigned int req_cap)
 	return rc;
 }
 
+#ifdef CONFIG_AUDIT
 struct selinux_audit_rule {
 	u32 au_seqno;
 	struct context au_ctxt;
@@ -3422,6 +3436,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
