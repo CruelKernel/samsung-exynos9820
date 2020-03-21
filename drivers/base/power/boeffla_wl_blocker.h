930ea66bf8521b37b3dcaa889fbde70a5e2f21bc
Denis Efremov <efremov@linux.com>
2020-03-21 03:27:53 +0300

    boeffla_wl_blocker: add default settings
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"mmc0_detect"
+#define LIST_WL_DEFAULT			"19030000.decon_f;ssp_comm_wake_lock;GPSD;ssp_wake_lock;tsp_wakelock;umts_ipc0;umts_ipc1;mmc0_detect"
 
-#define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		150
+#define LENGTH_LIST_WL			2048
+#define LENGTH_LIST_WL_DEFAULT		255
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
