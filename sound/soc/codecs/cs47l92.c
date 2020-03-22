aa05de7922638d7e9e9cdfdf3c6039562ca2dd59
Nico Becker <nicistyler98@gmail.com>
2020-03-22 04:31:51 +0100

    sound: add moro sound module
    Added Moro Sound Module
    
    Signed-off-by: Nico Becker <nicistyler98@gmail.com>
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- sound/soc/codecs/cs47l92.c
+++ sound/soc/codecs/cs47l92.c
@@ -30,6 +30,10 @@
 #include "madera.h"
 #include "wm_adsp.h"
 
+#ifdef CONFIG_MORO_SOUND
+#include "moro_sound.h"
+#endif
+
 #define CS47L92_NUM_ADSP	1
 #define CS47L92_MONO_OUTPUTS	3
 
@@ -1973,6 +1977,12 @@ static int cs47l92_codec_probe(struct snd_soc_codec *codec)
 	unsigned int val = 0;
 
 	madera->dapm = snd_soc_codec_get_dapm(codec);
+
+#ifdef CONFIG_MORO_SOUND
+	moro_sound_hook_madera_pcm_probe(madera->regmap);
+
+	cs47l92->core.madera->dapm = snd_soc_codec_get_dapm(codec);
+#endif
 
 	ret = madera_init_inputs(codec,
 				 cs47l92_dmic_inputs,
