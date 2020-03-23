8186d2a63537c7faf5de6bf5ac5091e75bd89c20
hrtkernel <HRTKernel@users.noreply.github.com>
2020-03-23 15:21:21 +0100

    mmc:
    disable CRC check for better performance
    spi CRC configurable under /sys/module/mmc_core/parameters/use_spi_crc

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -69,8 +69,8 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-bool use_spi_crc = 1;
-module_param(use_spi_crc, bool, 0);
+bool use_spi_crc = 0;
+module_param(use_spi_crc, bool, 0644);
 
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
