fdd49c00b88fab5ef96225f95501f0d5aea58934
Extreme Grief <extremegrief@pop-os.localdomain>
2020-04-12 22:31:39 +0300

    Decrease swappiness

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -151,7 +151,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 8;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
