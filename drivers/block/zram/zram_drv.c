88aea2002ab15da1338dec4ef99bcb61a7fa723d
Extreme Grief <extremegrief@pop-os.localdomain>
2020-04-13 12:31:57 +0300

    zram: force use of LZ4

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -43,13 +43,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-#if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 static const char *default_compressor = "lz4";
-#elif IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-static const char *default_compressor = "zstd";
-#else
-static const char *default_compressor = "lzo";
-#endif
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
