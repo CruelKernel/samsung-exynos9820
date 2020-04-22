1d70a9e1da39fc3f55d5e29e60e73e8f7c6c05d9
Denis Efremov <efremov@linux.com>
2020-04-22 17:50:32 +0300

    N976N - CTCB
    Signed-off-by: Denis Efremov <efremov@linux.com>

---
+++ arch/arm64/boot/dts/N976N.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0
+dtbo-y += samsung/exynos9820-d2x_kor_21.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_16.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_02.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_22.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_19.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_18.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_17.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_23.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_24.dtbo
+dtb-y += exynos/exynos9825-old.dtb
+
+targets += dtbs
+DTB_LIST  := $(dtb-y) $(dtbo-y)
+always    := $(DTB_LIST)
+
+dtbs: $(addprefix $(obj)/, $(DTB_LIST))
+
+clean-files := *.dtb*
