838147847ffaeab53c03394901473d9d7bdc5469
Danny Lin <danny@kdrag0n.dev>
2020-07-02 16:27:13 -0700

    vmlinux.lds.h: Coalesce transient LLVM dead code elimination sections
    A recent LLVM 11 commit [1] made LLD stop implicitly coalescing some
    temporary LLVM sections, namely .{data,bss}..compoundliteral.XXX:
    
      [30] .data..compoundli PROGBITS         ffffffff9ac9a000  19e9a000
           000000000000cea0  0000000000000000  WA       0     0     32
      [31] .rela.data..compo RELA             0000000000000000  40965440
           0000000000001d88  0000000000000018   I      2238    30     8
      [32] .data..compoundli PROGBITS         ffffffff9aca6ea0  19ea6ea0
           00000000000033c0  0000000000000000  WA       0     0     32
      [33] .rela.data..compo RELA             0000000000000000  409671c8
           0000000000000948  0000000000000018   I      2238    32     8
      [...]
      [2213] .bss..compoundlit NOBITS           ffffffffa3000000  1d85c000
           00000000000000a0  0000000000000000  WA       0     0     32
      [2214] .bss..compoundlit NOBITS           ffffffffa30000a0  1d85c000
           0000000000000040  0000000000000000  WA       0     0     32
      [...]
    
    .{data,bss}..L<symbol name> sections are also created in some cases.
    While there aren't any in this example, they should also be coalesced to
    be safe in case some config or future LLVM change makes it start
    creating more of those sections in the future. For example, enabling
    global merging causes ..L_MergedGlobals sections to be created, but it's
    likely that other changes will result in such sections as well.
    
    While these extra sections don't typically cause any breakage, they do
    inflate the vmlinux size due to the overhead of storing metadata for
    thousands of extra sections.
    
    It's also worth noting that for some reason, some downstream Android
    kernels can't boot at all if these sections aren't coalesced.
    
    This issue isn't limited to any specific architecture; it affects arm64
    and x86 if CONFIG_LD_DEAD_CODE_DATA_ELIMINATION is forced on.
    
    Example on x86 allyesconfig:
        Before: 2241 sections, 1171169 KiB
        After:    56 sections, 1170972 KiB
    
    [1] https://github.com/llvm/llvm-project/commit/9e33c096476ab5e02ab1c8442cc3cb4e32e29f17
    
    Suggested-by: Fangrui Song <maskray@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Reviewed-by: Nathan Chancellor <natechancellor@gmail.com>
    Cc: stable@vger.kernel.org # v4.4+
    Link: https://github.com/ClangBuiltLinux/linux/issues/958

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -72,9 +72,12 @@
 #define TEXT_MAIN .text .text.[0-9a-zA-Z_]*
 #define TEXT_CFI_MAIN .text.cfi .text.[0-9a-zA-Z_]*.cfi
 #define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..LPBX*
+#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..LPBX* \
+		  .data..compoundliteral* .data..L*
 #define SDATA_MAIN .sdata .sdata.[0-9a-zA-Z_]*
 #define RODATA_MAIN .rodata .rodata.[0-9a-zA-Z_]*
 #define BSS_MAIN .bss .bss.[0-9a-zA-Z_]*
+#define BSS_MAIN .bss .bss.[0-9a-zA-Z_]* .bss..compoundliteral* .bss..L*
 #define SBSS_MAIN .sbss .sbss.[0-9a-zA-Z_]*
 #else
 #define TEXT_MAIN .text
