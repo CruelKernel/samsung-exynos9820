63a5f8907d0c8edfbdb26c70ae0ac4c0a11d2f1c
googyanas <anas.reggoug@gmail.com>
2020-07-05 20:10:14 +0100

    fs: enforce noatime mount flag

--- fs/namespace.c
+++ fs/namespace.c
@@ -3417,10 +3417,13 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 	if (retval)
 		goto dput_out;
 
-	/* Default to relatime unless overriden */
-	if (!(flags & MS_NOATIME))
-		mnt_flags |= MNT_RELATIME;
+//    /* Default to relatime unless overriden */
+//	if (!(flags & MS_NOATIME))
+//		mnt_flags |= MNT_RELATIME;
 
+    /* enforce noatime */
+    mnt_flags |= MNT_NOATIME;
+    
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
 		mnt_flags |= MNT_NOSUID;
