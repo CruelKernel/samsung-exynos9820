8be9d8e05efc8af79eb146624c875e31c1a2d383
Denis Efremov <efremov@linux.com>
2020-09-10 18:56:07 +0300

    spu-verify: add stub for spu_firmware_signature_verify()
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- include/linux/spu-verify.h
+++ include/linux/spu-verify.h
@@ -26,6 +26,40 @@
 /* TOTAL METADATA SIZE */
 #define SPU_METADATA_SIZE(FW) 	( (TAG_LEN(FW)) + (DIGEST_LEN) + (SIGN_LEN) )
 
+#ifdef CONFIG_SPU_VERIFY
 extern long spu_firmware_signature_verify(const char* fw_name, const u8* fw_data, const long fw_size);
+#else
+static inline long spu_firmware_signature_verify(const char* fw_name, const u8* fw_data, const long fw_size) {
+	const static struct {
+		const char *tag;
+		int len;
+		int metadata_size;
+	} tags[] = {
+		{ TSP_TAG,       TAG_LEN(TSP),       SPU_METADATA_SIZE(TSP) },
+		{ MFC_TAG,       TAG_LEN(MFC),       SPU_METADATA_SIZE(MFC) },
+		{ WACOM_TAG,     TAG_LEN(WACOM),     SPU_METADATA_SIZE(WACOM) },
+		{ PDIC_TAG,      TAG_LEN(PDIC),      SPU_METADATA_SIZE(PDIC) },
+		{ SENSORHUB_TAG, TAG_LEN(SENSORHUB), SPU_METADATA_SIZE(SENSORHUB) },
+	};
+	int i;
+
+	if (!fw_name || !fw_data || fw_size < 0) {
+		return -EINVAL;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(tags); ++i) {
+		if(!strncmp(fw_name, tags[i].tag, tags[i].len)) {
+			long offset = fw_size - tags[i].metadata_size;
+			if (!strncmp(fw_name, fw_data + offset, tags[i].len)) {
+				return offset;
+			} else {
+				return -EINVAL;
+			}
+		}
+	}
+
+	return -EINVAL;
+}
+#endif
 
 #endif //end _SPU_VERIFY_H_
