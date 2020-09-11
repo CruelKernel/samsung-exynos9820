c2ee3852502818405e26e281d93b7d96e9375dff
Denis Efremov <efremov@linux.com>
2020-09-11 11:21:09 +0300

    G970F - DTH7
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G970F.mk
+++ arch/arm64/boot/dts/G970F.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0
 dtbo-y += samsung/exynos9820-beyond0lte_eur_open_24.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_25.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_22.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_19.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_18.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_eur_open_20.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_eur_open_17.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_18.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_22.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_19.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_25.dtbo
 dtb-y += exynos/exynos9820.dtb
 
 targets += dtbs
