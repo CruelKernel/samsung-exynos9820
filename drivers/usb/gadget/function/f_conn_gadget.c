fff5fd375a86a175adb377516fe491266aae6c65
Denis Efremov <efremov@linux.com>
2020-09-14 11:36:51 +0300

    drivers/usb/gadget/function/f_conn_gadget: fix misleading indentation
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/usb/gadget/function/f_conn_gadget.c
+++ drivers/usb/gadget/function/f_conn_gadget.c
@@ -1284,8 +1284,8 @@ static int conn_gadget_setup(struct conn_gadget_instance *fi_conn_gadget)
 	return 0;
 err_:
 
-    if (dev->rd_queue_buf)
-	vfree(dev->rd_queue_buf);
+	if (dev->rd_queue_buf)
+		vfree(dev->rd_queue_buf);
 
 	_conn_gadget_dev = NULL;
 	kfree(dev);
@@ -1304,8 +1304,8 @@ static void conn_gadget_cleanup(struct kref *kref)
 
 	misc_deregister(&conn_gadget_device);
 
-    if (_conn_gadget_dev->rd_queue_buf)
-	vfree(_conn_gadget_dev->rd_queue_buf);
+	if (_conn_gadget_dev->rd_queue_buf)
+		vfree(_conn_gadget_dev->rd_queue_buf);
 
 	kfree(_conn_gadget_dev);
 	_conn_gadget_dev = NULL;
