b8684861c037907a11c065cfbec6bf9ca4399df1
Denis Efremov <efremov@linux.com>
2020-10-26 17:42:44 +0300

    fs: add reiser4 filesystem
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- include/linux/writeback.h
+++ include/linux/writeback.h
@@ -16,6 +16,12 @@ struct bio;
 
 DECLARE_PER_CPU(int, dirty_throttle_leaks);
 
+static inline int is_flush_bd_task(struct task_struct *task)
+{
+       return task->flags & PF_FLUSHER;
+}
+#define current_is_flush_bd_task()  is_flush_bd_task(current)
+
 /*
  * The 1/4 region under the global dirty thresh is for smooth dirty throttling:
  *
@@ -178,6 +184,26 @@ static inline void wb_domain_size_changed(struct wb_domain *dom)
 	dom->dirty_limit = 0;
 	spin_unlock(&dom->lock);
 }
+
+/*
+ * Passed into wb_writeback(), essentially a subset of writeback_control
+ */
+struct wb_writeback_work {
+	long nr_pages;
+	struct super_block *sb;
+	unsigned long *older_than_this;
+	enum writeback_sync_modes sync_mode;
+	unsigned int tagged_writepages:1;
+	unsigned int for_kupdate:1;
+	unsigned int range_cyclic:1;
+	unsigned int for_background:1;
+	unsigned int for_sync:1;	/* sync(2) WB_SYNC_ALL writeback */
+	unsigned int auto_free:1;	/* free on completion */
+	enum wb_reason reason;		/* why was writeback initiated? */
+
+	struct list_head list;		/* pending work list */
+	struct wb_completion *done;	/* set if the caller waits */
+};
 
 /*
  * fs/fs-writeback.c
