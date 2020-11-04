2d37b167c0bc87ea1032df511292a4fb34c70459
Denis Efremov <efremov@linux.com>
2020-11-04 09:04:53 +0300

    G973N - DTH7
    Signed-off-by: Denis Efremov <efremov@linux.com>

---
+++ arch/arm64/boot/dts/G973N.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0
+dtb-y += exynos/exynos9820.dtb
+dtbo-y += samsung/exynos9820-beyond1lte_kor_17.dtbo
+dtbo-y += samsung/exynos9820-beyond1lte_kor_21.dtbo
+dtbo-y += samsung/exynos9820-beyond1lte_kor_26.dtbo
+dtbo-y += samsung/exynos9820-beyond1lte_kor_20.dtbo
+dtbo-y += samsung/exynos9820-beyond1lte_kor_18.dtbo
+dtbo-y += samsung/exynos9820-beyond1lte_kor_19.dtbo
+
+targets += dtbs
+DTB_LIST  := $(dtb-y) $(dtbo-y)
+always    := $(DTB_LIST)
+
+dtbs: $(addprefix $(obj)/, $(DTB_LIST))
+
+clean-files := *.dtb*
