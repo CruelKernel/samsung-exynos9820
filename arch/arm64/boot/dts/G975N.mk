79bd01d6574a1e2e45993d529a3a3a27bf10f686
Denis Efremov <efremov@linux.com>
2020-11-23 14:10:43 +0300

    G975N - DTJA
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G975N.mk
+++ arch/arm64/boot/dts/G975N.mk
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
-dtb-y += exynos/exynos9820.dtb
-dtbo-y += samsung/exynos9820-beyond2lte_kor_17.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_kor_20.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_kor_18.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_kor_19.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_kor_26.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_kor_25.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_kor_20.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_kor_17.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_kor_24.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_kor_26.dtbo
+dtb-y += exynos/exynos9820.dtb
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
