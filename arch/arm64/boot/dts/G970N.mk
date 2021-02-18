759ab52b7d6568a9ddd925e48642d18c8eac54b0
Denis Efremov <efremov@linux.com>
2021-02-18 08:38:03 +0300

    G970N - EUA4
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G970N.mk
+++ arch/arm64/boot/dts/G970N.mk
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
-dtbo-y += samsung/exynos9820-beyond0lte_kor_17.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_kor_18.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_kor_20.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_kor_18.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_kor_19.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_kor_25.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_kor_17.dtbo
 dtb-y += exynos/exynos9820.dtb
 
 targets += dtbs
