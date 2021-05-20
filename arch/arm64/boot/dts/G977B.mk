4f14c92e94eef795493b1f68de3de28c6d859b2b
Denis Efremov <efremov@linux.com>
2021-05-20 20:09:21 +0300

    G977B - FUE3
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G977B.mk
+++ arch/arm64/boot/dts/G977B.mk
@@ -1,14 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0
 dtb-y += exynos/exynos9820.dtb
-dtbo-y += samsung/exynos9820-beyondx_eur_open_00.dtbo
-dtbo-y += samsung/exynos9820-beyondx_eur_open_08.dtbo
-dtbo-y += samsung/exynos9820-beyondx_eur_open_04.dtbo
-dtbo-y += samsung/exynos9820-beyondx_eur_open_06.dtbo
-dtbo-y += samsung/exynos9820-beyondx_eur_open_01.dtbo
 dtbo-y += samsung/exynos9820-beyondx_eur_open_05.dtbo
+dtbo-y += samsung/exynos9820-beyondx_eur_open_06.dtbo
+dtbo-y += samsung/exynos9820-beyondx_eur_open_08.dtbo
+dtbo-y += samsung/exynos9820-beyondx_eur_open_03.dtbo
+dtbo-y += samsung/exynos9820-beyondx_eur_open_04.dtbo
+dtbo-y += samsung/exynos9820-beyondx_eur_open_00.dtbo
+dtbo-y += samsung/exynos9820-beyondx_eur_open_01.dtbo
 dtbo-y += samsung/exynos9820-beyondx_eur_open_02.dtbo
 dtbo-y += samsung/exynos9820-beyondx_eur_open_07.dtbo
-dtbo-y += samsung/exynos9820-beyondx_eur_open_03.dtbo
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
