a121dedc00018a54f438b642a48f470bd054f83d
Denis Efremov <efremov@linux.com>
2021-06-28 12:00:53 +0300

    N976N - FUE3
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/N976N.mk
+++ arch/arm64/boot/dts/N976N.mk
@@ -1,14 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0
-dtbo-y += samsung/exynos9820-d2x_kor_23.dtbo
-dtbo-y += samsung/exynos9820-d2x_kor_16.dtbo
-dtbo-y += samsung/exynos9820-d2x_kor_22.dtbo
-dtbo-y += samsung/exynos9820-d2x_kor_21.dtbo
+dtb-y += exynos/exynos9825.dtb
 dtbo-y += samsung/exynos9820-d2x_kor_24.dtbo
-dtbo-y += samsung/exynos9820-d2x_kor_18.dtbo
-dtbo-y += samsung/exynos9820-d2x_kor_02.dtbo
 dtbo-y += samsung/exynos9820-d2x_kor_17.dtbo
 dtbo-y += samsung/exynos9820-d2x_kor_19.dtbo
-dtb-y += exynos/exynos9825.dtb
+dtbo-y += samsung/exynos9820-d2x_kor_02.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_16.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_21.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_22.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_18.dtbo
+dtbo-y += samsung/exynos9820-d2x_kor_23.dtbo
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
