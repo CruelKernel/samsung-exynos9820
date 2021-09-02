eb7a39f67876c7e88e1611fb423d9ffcd7d188c9
Denis Efremov <efremov@linux.com>
2021-09-02 13:15:16 +0300

    G975F - FUH3
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G975F.mk
+++ arch/arm64/boot/dts/G975F.mk
@@ -1,15 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_16.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_17.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_19.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_23.dtbo
+dtb-y += exynos/exynos9820.dtb
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_20.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_04.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_eur_open_26.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_17.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_23.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_25.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_eur_open_18.dtbo
 dtbo-y += samsung/exynos9820-beyond2lte_eur_open_24.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_04.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_20.dtbo
-dtbo-y += samsung/exynos9820-beyond2lte_eur_open_25.dtbo
-dtb-y += exynos/exynos9820.dtb
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_19.dtbo
+dtbo-y += samsung/exynos9820-beyond2lte_eur_open_16.dtbo
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
