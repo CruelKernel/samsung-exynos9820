977bdb3118eb636e43a074b19a95823b20031370
Denis Efremov <efremov@linux.com>
2022-01-06 14:28:27 +0300

    G970F - GULB
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G970F.mk
+++ arch/arm64/boot/dts/G970F.mk
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
-dtb-y += exynos/exynos9820.dtb
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_24.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_22.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_19.dtbo
-dtbo-y += samsung/exynos9820-beyond0lte_eur_open_18.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_eur_open_17.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_eur_open_20.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_24.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_18.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_19.dtbo
+dtbo-y += samsung/exynos9820-beyond0lte_eur_open_22.dtbo
 dtbo-y += samsung/exynos9820-beyond0lte_eur_open_25.dtbo
+dtb-y += exynos/exynos9820.dtb
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
