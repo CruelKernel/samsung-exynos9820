bec4428a273a01c7a085d4c0a050ce7e57c5d727
Denis Efremov <efremov@linux.com>
2022-01-10 15:57:57 +0300

    N976B - GULD
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/N976B.mk
+++ arch/arm64/boot/dts/N976B.mk
@@ -1,15 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0
-dtb-y += exynos/exynos9825.dtb
-dtbo-y += samsung/exynos9820-d2x_eur_open_20.dtbo
-dtbo-y += samsung/exynos9820-d2x_eur_open_19.dtbo
-dtbo-y += samsung/exynos9820-d2x_eur_open_17.dtbo
-dtbo-y += samsung/exynos9820-d2x_eur_open_24.dtbo
 dtbo-y += samsung/exynos9820-d2x_eur_open_16.dtbo
-dtbo-y += samsung/exynos9820-d2x_eur_open_21.dtbo
-dtbo-y += samsung/exynos9820-d2x_eur_open_02.dtbo
+dtbo-y += samsung/exynos9820-d2x_eur_open_19.dtbo
 dtbo-y += samsung/exynos9820-d2x_eur_open_18.dtbo
+dtbo-y += samsung/exynos9820-d2x_eur_open_20.dtbo
 dtbo-y += samsung/exynos9820-d2x_eur_open_22.dtbo
+dtbo-y += samsung/exynos9820-d2x_eur_open_17.dtbo
+dtbo-y += samsung/exynos9820-d2x_eur_open_21.dtbo
+dtbo-y += samsung/exynos9820-d2x_eur_open_24.dtbo
+dtbo-y += samsung/exynos9820-d2x_eur_open_02.dtbo
 dtbo-y += samsung/exynos9820-d2x_eur_open_23.dtbo
+dtb-y += exynos/exynos9825.dtb
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
