0874a6fd9666b7670baa2e69b4a5b72bc655283b
Denis Efremov <efremov@linux.com>
2022-03-31 10:32:53 +0300

    N975F - HVC6
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/misc/modem_v1_dual/modem_io_device.c
+++ drivers/misc/modem_v1_dual/modem_io_device.c
@@ -1507,7 +1507,7 @@ static struct net_device_ops vnet_ops = {
 static void vnet_setup(struct net_device *ndev)
 {
 	ndev->netdev_ops = &vnet_ops;
-	ndev->type = ARPHRD_PPP;
+	ndev->type = ARPHRD_RAWIP;
 	ndev->flags = IFF_POINTOPOINT | IFF_NOARP | IFF_MULTICAST;
 	ndev->addr_len = 0;
 	ndev->hard_header_len = 0;
