c5af48e9588c57d02c90bf877670d4795c63b0c4
Denis Efremov <efremov@linux.com>
2022-09-04 11:55:40 +0400

    G977N - HVG5
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/G977N.mk
+++ arch/arm64/boot/dts/G977N.mk
@@ -1,14 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0
 dtb-y += exynos/exynos9820.dtb
-dtbo-y += samsung/exynos9820-beyondx_kor_08.dtbo
 dtbo-y += samsung/exynos9820-beyondx_kor_03.dtbo
-dtbo-y += samsung/exynos9820-beyondx_kor_07.dtbo
 dtbo-y += samsung/exynos9820-beyondx_kor_06.dtbo
 dtbo-y += samsung/exynos9820-beyondx_kor_04.dtbo
+dtbo-y += samsung/exynos9820-beyondx_kor_07.dtbo
 dtbo-y += samsung/exynos9820-beyondx_kor_01.dtbo
-dtbo-y += samsung/exynos9820-beyondx_kor_00.dtbo
-dtbo-y += samsung/exynos9820-beyondx_kor_05.dtbo
 dtbo-y += samsung/exynos9820-beyondx_kor_02.dtbo
+dtbo-y += samsung/exynos9820-beyondx_kor_08.dtbo
+dtbo-y += samsung/exynos9820-beyondx_kor_05.dtbo
+dtbo-y += samsung/exynos9820-beyondx_kor_00.dtbo
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
