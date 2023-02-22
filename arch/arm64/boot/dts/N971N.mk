4162b4bd879d1fcde6382967e66c1ea407aa6789
Denis Efremov <efremov@linux.com>
2023-02-22 15:40:21 +0400

    N971N - HWA2
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- arch/arm64/boot/dts/N971N.mk
+++ arch/arm64/boot/dts/N971N.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
-dtb-y += exynos/exynos9825.dtb
-dtbo-y += samsung/exynos9820-d1x_kor_22.dtbo
-dtbo-y += samsung/exynos9820-d1x_kor_18.dtbo
-dtbo-y += samsung/exynos9820-d1x_kor_23.dtbo
 dtbo-y += samsung/exynos9820-d1x_kor_19.dtbo
+dtbo-y += samsung/exynos9820-d1x_kor_22.dtbo
 dtbo-y += samsung/exynos9820-d1x_kor_21.dtbo
+dtbo-y += samsung/exynos9820-d1x_kor_23.dtbo
+dtbo-y += samsung/exynos9820-d1x_kor_18.dtbo
+dtb-y += exynos/exynos9825.dtb
 
 targets += dtbs
 DTB_LIST  := $(dtb-y) $(dtbo-y)
