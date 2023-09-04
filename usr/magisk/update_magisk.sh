41aba77656e3377e8c55691b8b807d915e545343
Denis Efremov <efremov@linux.com>
2023-09-04 20:04:21 +0400

    magisk: Fix broken link for magisk v26.3
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -15,12 +15,16 @@ then
 	nver="alpha"
 	magisk_link="https://github.com/vvb2060/magisk_files/raw/${nver}/app-release.apk"
 else
+	dash='-'
 	if [ "x$1" = "x" ]; then
 		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
 	else
 		nver="$1"
 	fi
-	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk-${nver}.apk"
+	if [ "$nver" = "v26.3" ]; then
+		dash='.'
+	fi
+	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk${dash}${nver}.apk"
 fi
 
 if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/magiskinit" \) -o \( "$nver" = "canary" \) -o \( "$nver" = "alpha" \) ]
